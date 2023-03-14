f316233257bcfc57ade95b17807a0cc2a9c9db06
yubraj13 <yubrajbashyal13@gmail.com>
2023-03-14 19:55:15 +0545

    UI changes

--- frontend/src/Admin/NodeManage.js
+++ frontend/src/Admin/NodeManage.js
@@ -10,50 +10,58 @@ const NodeManage = (props) => {
   const [refresh, setRefresh] = useState("");
 
   useEffect(() => {
-    getNetworkNodes(token).then((response) => {
-      console.log(response);
-      setNodes(response.data);
-    });
+    getNetworkNodes(token)
+      .then((response) => {
+        setNodes(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }, [token, refresh]);
 
   const submitHandler = (e) => {
     e.preventDefault();
-    const newNodeUrl = "http://" + ip + ":" + port;
-    registeAndBroadcastNode(newNodeUrl, token).then((response) => {
-      console.log(response);
-      setAlert(response.data.note);
-      setRefresh(Math.random());
-    });
+    const newNodeUrl = `http://${ip}:${port}`;
+    registeAndBroadcastNode(newNodeUrl, token)
+      .then((response) => {
+        setAlert(response.data.note);
+        setRefresh(Math.random());
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   };
+
   return (
     <div className="node-container">
       <form onSubmit={submitHandler}>
-        <label>IP Addres</label>
+        <label htmlFor="ip">IP Address</label>
         <input
           type="text"
+          id="ip"
           required
           placeholder="Enter IP Address Of Running Node"
           onChange={(e) => setIp(e.target.value)}
         />
-        <label>PORT</label>
+        <label htmlFor="port">Port</label>
         <input
           type="number"
+          id="port"
           required
           placeholder="Enter Port Number"
           onChange={(e) => setPort(e.target.value)}
         />
         <button type="submit">Connect</button>
       </form>
-      <div class="alert">{alert}</div>
+      {alert && <div className="alert">{alert}</div>}
       <div className="manage-node">
         <h3>Current Nodes In The Network</h3>
         <ul>
-          {nodes.map((node) => {
-            return <li>{node}</li>;
-          })}
+          {nodes.map((node) => (
+            <li key={node}>{node}</li>
+          ))}
         </ul>
       </div>
-
     </div>
   );
 };
