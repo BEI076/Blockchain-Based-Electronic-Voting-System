384eda75097bfa21ba18f02a2a3f8b34daffa560
anil0403 <anilshrestha0403.as@gmail.com>
2023-03-22 00:34:52 -0700

    zookeeper locking modfifiend with sequence number

--- backend/config/locking.js
+++ backend/config/locking.js
@@ -1,17 +1,8 @@
-// const express = require("express");
-// const app = express();
-// const bodyParser = require("body-parser");
 const zookeeper = require("node-zookeeper-client");
 const os = require("os");
-
-// Configure body parser to handle JSON data
-// app.use(bodyParser.json());
-
-// Configure ZooKeeper connection settings
 const zkClient = zookeeper.createClient("192.168.1.92:2181");
-const lockPath = "/blockchain-lock-" + os.hostname();
+const lockPath = "/blockchain-lock-" + os.hostname() + "-seq-";
 
-// Connect to ZooKeeper ensemble
 zkClient.connect();
 zkClient.once("connected", () => {
   console.log("Connected to ZooKeeper ensemble");
@@ -22,7 +13,7 @@ module.exports = {
     return new Promise((resolve, reject) => {
       zkClient.create(
         lockPath,
-        zookeeper.CreateMode.EPHEMERAL,
+        zookeeper.CreateMode.EPHEMERAL_SEQUENTIAL,
         (error, path) => {
           if (error) {
             reject(error);
@@ -52,54 +43,24 @@ module.exports = {
     });
   },
   releaseLock: async () => {
-    zkClient.remove(lockPath, -1, (error, path) => {
+    zkClient.getChildren("/", false, (error, children) => {
       if (error) {
-        console.error("Failed to release lock:", error);
-      } else {
-        console.log("Lock released:", path);
+        console.error("Failed to get children:", error);
+        return;
+      }
+      const lockNodes = children.filter((child) => child.startsWith("blockchain-lock"));
+      lockNodes.sort();
+      if (lockNodes.length > 0) {
+        const firstLockNode = lockNodes[0];
+        const lockNodePath = "/" + firstLockNode;
+        zkClient.remove(lockNodePath, -1, (error, path) => {
+          if (error) {
+            console.error("Failed to release lock:", error);
+          } else {
+            console.log("Lock released:", path);
+          }
+        });
       }
     });
   },
 };
-// Acquire a lock in ZooKeeper
-// function acquireLock() {
-//   console.log("locked");
-// }
-
-// // Release the lock in ZooKeeper
-// function releaseLock(lockPath) {
-//   zkClient.remove(lockPath, -1, (error, path) => {
-//     if (error) {
-//       console.error("Failed to release lock:", error);
-//     } else {
-//       console.log("Lock released:", path);
-//     }
-//   });
-// }
-
-// Add a block to the blockchain
-// app.post("/test", async (req, res) => {
-//   try {
-//     const lockPath = await acquireLock();
-//     // Perform critical section of code here
-//     console.log("Performing actions");
-//     setTimeout(() => {
-//       releaseLock(lockPath);
-//     }, 5000);
-//     res.status(200).json({ message: "Block added to blockchain" });
-//   } catch (error) {
-//     console.error("Error acquiring lock:", error);
-//     res.status(500).json({ error: "Failed to add block to blockchain" });
-//   }
-// });
-
-// app.get("/api", async (req, res) => {
-//   return res.json({
-//     message: "Hello World",
-//   });
-// });
-
-// // Start the ExpressJS server
-// app.listen(3002, () => {
-//   console.log("ExpressJS server running on port 3002");
-// });
